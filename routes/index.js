00c512aea123aae75562524cd38b11c9b0f48859
Ruben Calderon <Rcaldeon0907@gmail.com>
2024-01-31 19:30:40 -0800

    .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
+const { func } = require("prop-types");
 var router = express.Router();
-
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express", messages });
 });
 
+router.get("/new", function (req, res) {
+  res.render("form", { messages: messages });
+});
+
+router.post("/new", function (req, res) {
+  messages.push({ text: req.body.msgText });
+  res.redirect("/");
+});
 module.exports = router;
